a6242b19df205106efdcd3b2a01f542a817e2a23
sebastiancaicedo <sebastiancaicedo@uninorte.edu.co>
2018-04-27 22:14:28 -0500

    initial commit

---
+++ Assets/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.transform;
+        var health = hit.root.GetComponentInChildren<Health>();
+        if (health != null)
+        {
+            health.TakeDamage(10);
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Health.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Health : NetworkBehaviour {
+
+    public const int MAX_HEALTH = 100;
+
+    [SyncVar(hook = "OnChangeHealth")]
+    public int currentHealth = MAX_HEALTH;
+    public Slider healtBar;
+    public bool destroyOnDeath;
+
+    private NetworkStartPosition[] spawnPoints;
+
+    private void Start()
+    {
+        if (!isLocalPlayer) return;
+
+        spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (!isServer) return;
+
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            if (destroyOnDeath)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                currentHealth = MAX_HEALTH;
+
+                // called on the Server, but invoked on the Clients
+                RpcRespawn();
+            }
+        }
+    }
+
+    //Hook
+    void OnChangeHealth(int currentHealth)
+    {
+        print(currentHealth);
+        healtBar.value = (float)currentHealth / MAX_HEALTH;
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if (isLocalPlayer)
+        {
+            Vector3 spawnPoint = Vector3.zero;
+
+            if(spawnPoints != null && spawnPoints.Length > 0)
+                spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+
+
+            transform.position = spawnPoint;
+        }
+    }
+
+}
